925f70854144ca1cc74d3c7a4f60127179d9f25e
zongm <zongmingyi1999@163.com>
2024-05-16 15:54:12 +0800

    添加信息

---
+++ src/main/java/manageSystem/manage/Delete.java
@@ -0,0 +1,12 @@
+package manageSystem.manage;
+
+import java.io.Serializable;
+
+public class Delete implements Serializable {
+    private static final long serialVersionUID = -8972359586288947347L;
+
+    public void delete(String id) {
+        String file = "information/scores";
+        
+    }
+}
